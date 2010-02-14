6e6c9e46b966264c1e79a22b7c7221355e7db84a
Chris Mear <chris@feedmechocolate.com>
2010-02-14 16:30:20 +0000

    Update gem versions.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -35,14 +35,14 @@ dependency "faker", "0.3.1"
 dependency "haml", ">= 2.2.5"
 dependency "erubis", ">= 2.6.5"
 dependency "extlib", ">= 0.9.13"
-dependency "rspec", "1.2.8", :require_as => nil
-dependency "rack", "1.0.0"
+dependency "rspec", ">=1.2.8", :require_as => nil
+dependency "rack", ">=1.0.0"
 dependency "fastthread", ">= 1.0.7"
 dependency "gem_plugin", "0.2.3"
 dependency "cgi_multipart_eof_fix", "2.5.0"
 dependency "mongrel", "1.1.5"
 dependency "data_objects", ">= 0.9.11"
-dependency "json_pure", "1.1.9", :require_as => 'json'
+dependency "json_pure", ">=1.1.9", :require_as => 'json'
 dependency "daemons", "1.0.10"
 dependency "thor", "0.9.9"
  
