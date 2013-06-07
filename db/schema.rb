1200babde4357a020ce6ffef0c3a62b94ec64288
Chris Mear <chris@feedmechocolate.com>
2013-06-07 21:41:55 +0100

    Update schema.rb with Rails 4/Ruby 1.9 syntax.

--- db/schema.rb
+++ db/schema.rb
@@ -9,118 +9,118 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140112175011) do
+ActiveRecord::Schema.define(version: 20140112175011) do
 
-  create_table "administrators", :force => true do |t|
-    t.string   "email",               :limit => 50, :null => false
-    t.string   "crypted_password",    :limit => 50
-    t.string   "salt",                :limit => 50
+  create_table "administrators", force: true do |t|
+    t.string   "email",               limit: 50, null: false
+    t.string   "crypted_password",    limit: 50
+    t.string   "salt",                limit: 50
     t.string   "password_reset_code"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
-  create_table "agenda_items", :force => true do |t|
+  create_table "agenda_items", force: true do |t|
     t.integer  "meeting_id"
     t.integer  "position"
     t.string   "title"
     t.text     "minutes"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "ballots", :force => true do |t|
+  create_table "ballots", force: true do |t|
     t.integer  "election_id"
     t.integer  "member_id"
     t.text     "ranking"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "clauses", :force => true do |t|
-    t.string   "name",                                                        :null => false
+  create_table "clauses", force: true do |t|
+    t.string   "name",                                                        null: false
     t.datetime "started_at"
     t.datetime "ended_at"
     t.text     "text_value"
     t.integer  "integer_value"
-    t.integer  "boolean_value",   :limit => 1
+    t.integer  "boolean_value",   limit: 1
     t.integer  "organisation_id"
-    t.decimal  "decimal_value",                :precision => 10, :scale => 5
+    t.decimal  "decimal_value",                precision: 10, scale: 5
   end
 
-  add_index "clauses", ["organisation_id"], :name => "index_clauses_on_organisation_id"
+  add_index "clauses", ["organisation_id"], name: "index_clauses_on_organisation_id", using: :btree
 
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: true do |t|
     t.integer  "author_id"
     t.integer  "commentable_id"
     t.text     "body"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "commentable_type"
   end
 
-  add_index "comments", ["commentable_type", "commentable_id"], :name => "index_comments_on_commentable_type_and_commentable_id"
+  add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
 
-  create_table "decisions", :force => true do |t|
+  create_table "decisions", force: true do |t|
     t.integer "proposal_id"
   end
 
-  add_index "decisions", ["proposal_id"], :name => "index_decisions_on_proposal_id"
+  add_index "decisions", ["proposal_id"], name: "index_decisions_on_proposal_id", using: :btree
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
     t.string   "locked_by"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "queue"
   end
 
-  create_table "directorships", :force => true do |t|
+  create_table "directorships", force: true do |t|
     t.integer  "organisation_id"
     t.integer  "director_id"
     t.date     "elected_on"
     t.date     "ended_on"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "documents", :force => true do |t|
+  create_table "documents", force: true do |t|
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "elections", :force => true do |t|
+  create_table "elections", force: true do |t|
     t.integer  "organisation_id"
     t.string   "state"
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "meeting_id"
     t.date     "nominations_closing_date"
     t.date     "voting_closing_date"
     t.integer  "seats"
   end
 
-  create_table "meeting_participations", :force => true do |t|
+  create_table "meeting_participations", force: true do |t|
     t.integer  "meeting_id"
     t.integer  "participant_id"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "meetings", :force => true do |t|
+  create_table "meetings", force: true do |t|
     t.date     "happened_on"
     t.text     "minutes"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "organisation_id"
     t.integer  "creator_id"
     t.string   "type"
@@ -129,15 +129,15 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.text     "agenda"
   end
 
-  create_table "member_classes", :force => true do |t|
-    t.string  "name",            :null => false
+  create_table "member_classes", force: true do |t|
+    t.string  "name",            null: false
     t.string  "description"
     t.integer "organisation_id"
   end
 
-  add_index "member_classes", ["organisation_id"], :name => "index_member_classes_on_organisation_id"
+  add_index "member_classes", ["organisation_id"], name: "index_member_classes_on_organisation_id", using: :btree
 
-  create_table "member_state_transitions", :force => true do |t|
+  create_table "member_state_transitions", force: true do |t|
     t.integer  "member_id"
     t.string   "event"
     t.string   "from"
@@ -145,13 +145,13 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.datetime "created_at"
   end
 
-  add_index "member_state_transitions", ["member_id"], :name => "index_member_state_transitions_on_member_id"
+  add_index "member_state_transitions", ["member_id"], name: "index_member_state_transitions_on_member_id"
 
-  create_table "members", :force => true do |t|
-    t.string   "email",               :limit => 50, :null => false
+  create_table "members", force: true do |t|
+    t.string   "email",               limit: 50, null: false
     t.datetime "created_at"
-    t.string   "crypted_password",    :limit => 50
-    t.string   "salt",                :limit => 50
+    t.string   "crypted_password",    limit: 50
+    t.string   "salt",                limit: 50
     t.integer  "organisation_id"
     t.integer  "member_class_id"
     t.datetime "inducted_at"
@@ -169,42 +169,42 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.string   "phone"
   end
 
-  add_index "members", ["organisation_id"], :name => "index_members_on_organisation_id"
+  add_index "members", ["organisation_id"], name: "index_members_on_organisation_id", using: :btree
 
-  create_table "nominations", :force => true do |t|
+  create_table "nominations", force: true do |t|
     t.integer  "election_id"
     t.integer  "nominee_id"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "state"
-    t.decimal  "votes",       :precision => 15, :scale => 10
+    t.decimal  "votes",       precision: 15, scale: 10
   end
 
-  create_table "officerships", :force => true do |t|
+  create_table "officerships", force: true do |t|
     t.integer  "office_id"
     t.integer  "officer_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.date     "elected_on"
     t.date     "ended_on"
   end
 
-  create_table "offices", :force => true do |t|
+  create_table "offices", force: true do |t|
     t.integer  "organisation_id"
     t.string   "title"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "organisations", :force => true do |t|
+  create_table "organisations", force: true do |t|
     t.string   "subdomain"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "type"
     t.string   "state"
   end
 
-  create_table "paragraphs", :force => true do |t|
+  create_table "paragraphs", force: true do |t|
     t.text     "body"
     t.integer  "position"
     t.integer  "parent_id"
@@ -213,18 +213,18 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.boolean  "continuation"
     t.string   "name"
     t.string   "topic"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.boolean  "list"
   end
 
-  create_table "proposals", :force => true do |t|
-    t.string   "title",                                     :null => false
+  create_table "proposals", force: true do |t|
+    t.string   "title",                                     null: false
     t.text     "description"
     t.datetime "creation_date"
     t.datetime "close_date"
-    t.string   "parameters",               :limit => 10000
-    t.string   "type",                     :limit => 50
+    t.string   "parameters",               limit: 10000
+    t.string   "type",                     limit: 50
     t.integer  "proposer_member_id"
     t.integer  "organisation_id"
     t.string   "state"
@@ -233,38 +233,38 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.integer  "additional_votes_against"
   end
 
-  create_table "resignations", :force => true do |t|
+  create_table "resignations", force: true do |t|
     t.integer  "member_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "seen_notifications", :force => true do |t|
+  create_table "seen_notifications", force: true do |t|
     t.integer  "member_id"
     t.string   "notification"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "seen_notifications", ["member_id", "notification"], :name => "index_seen_notifications_on_member_id_and_notification"
-  add_index "seen_notifications", ["member_id"], :name => "index_seen_notifications_on_member_id"
+  add_index "seen_notifications", ["member_id", "notification"], name: "index_seen_notifications_on_member_id_and_notification", using: :btree
+  add_index "seen_notifications", ["member_id"], name: "index_seen_notifications_on_member_id", using: :btree
 
-  create_table "settings", :force => true do |t|
+  create_table "settings", force: true do |t|
     t.string   "key"
     t.string   "value"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "share_accounts", :force => true do |t|
+  create_table "share_accounts", force: true do |t|
     t.integer  "owner_id"
     t.string   "owner_type"
     t.integer  "balance"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "share_transaction_state_transitions", :force => true do |t|
+  create_table "share_transaction_state_transitions", force: true do |t|
     t.integer  "share_transaction_id"
     t.string   "event"
     t.string   "from"
@@ -272,37 +272,37 @@ ActiveRecord::Schema.define(:version => 20140112175011) do
     t.datetime "created_at"
   end
 
-  add_index "share_transaction_state_transitions", ["share_transaction_id"], :name => "share_trans_state_trans_on_share_trans_id"
+  add_index "share_transaction_state_transitions", ["share_transaction_id"], name: "share_trans_state_trans_on_share_trans_id"
 
-  create_table "share_transactions", :force => true do |t|
+  create_table "share_transactions", force: true do |t|
     t.string   "state"
     t.integer  "amount"
     t.integer  "from_account_id"
     t.integer  "to_account_id"
     t.integer  "share_value"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "tasks", :force => true do |t|
+  create_table "tasks", force: true do |t|
     t.integer  "member_id"
     t.integer  "subject_id"
     t.string   "subject_type"
     t.string   "action"
     t.datetime "completed_at"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.date     "starts_on"
     t.datetime "dismissed_at"
   end
 
-  create_table "votes", :force => true do |t|
+  create_table "votes", force: true do |t|
     t.integer  "member_id"
     t.integer  "proposal_id"
-    t.integer  "for",         :limit => 1, :null => false
+    t.integer  "for",         limit: 1, null: false
     t.datetime "created_at"
   end
 
-  add_index "votes", ["proposal_id", "member_id"], :name => "index_votes_on_proposal_id_and_member_id"
+  add_index "votes", ["proposal_id", "member_id"], name: "index_votes_on_proposal_id_and_member_id", using: :btree
 
 end
